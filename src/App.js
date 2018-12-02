bdf51bf4427d1d9cddabfd5e20c76c40e1de8fc8
Dominik Chmielarz <d.chmielarz@gmail.com>
2018-12-02 13:22:14 +0100

    add number of swimmers to add select

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import AppContent from "./AppContent";
 import {createGlobalStyle} from "styled-components";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import "@blueprintjs/select/lib/css/blueprint-select.css";
 
 const GlobalStyle = createGlobalStyle`
     body {
--- src/components/settings-panel/SettingsPanel.js
+++ src/components/settings-panel/SettingsPanel.js
@@ -1,12 +1,18 @@
+import _ from "lodash";
 import React from "react";
 import styled from "styled-components";
 import * as PropTypes from "prop-types";
 import {Button} from "@blueprintjs/core";
+import {Select} from "@blueprintjs/select";
 import {IconNames} from "@blueprintjs/icons";
 import SwimmingPool from "../../model/SwimmingPool";
 
 const Container = styled.div`
     display: flex;
+`;
+const Item = styled.div`
+    cursor: pointer;
+    text-align: center;
 `;
 
 export default class SettingsPanel extends React.PureComponent {
@@ -15,13 +21,40 @@ export default class SettingsPanel extends React.PureComponent {
         swimmingPool: PropTypes.instanceOf(SwimmingPool)
     };
 
+    state = {
+        selectedNumberOfSwimmersToAdd: 10
+    };
+
+    renderItem = (item, props) => {
+        return (
+            <Item key={props.index} onClick={props.handleClick}>{item}</Item>
+        );
+    };
+
+    onItemChange = (item) => {
+        this.setState({
+            selectedNumberOfSwimmersToAdd: item
+        });
+    };
+
+    addSwimmers = () => {
+        _.times(this.state.selectedNumberOfSwimmersToAdd, this.props.swimmingPool.addSwimmer);
+    };
+
     render() {
         return (
             <Container>
+                <Select
+                    items={[1, 5, 10, 20]}
+                    filterable={false}
+                    itemRenderer={this.renderItem}
+                    onItemSelect={this.onItemChange}>
+                    <Button>{this.state.selectedNumberOfSwimmersToAdd}</Button>
+                </Select>
                 <Button
                     icon={IconNames.ADD}
-                    onClick={this.props.swimmingPool.addSwimmer}>
-                    add new swimmer
+                    onClick={this.addSwimmers}>
+                    add swimmers
                 </Button>
             </Container>
         );
