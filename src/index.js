fe9055fc73d57b9cc5e5d4f59c30f0ff1989e161
Dominik Chmielarz <d.chmielarz@gmail.com>
2020-08-28 22:54:38 +0200

    remove unused redux

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
-import {Provider} from "react-redux";
-import {createStore} from "redux";
-import rootReducer from "./redux/rootReducer";
 import registerServiceWorker from "./registerServiceWorker";
-import {composeWithDevTools} from "redux-devtools-extension";
-
-const store = createStore(rootReducer, composeWithDevTools());
-
-function renderRootElement() {
-    return (
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    );
-}
 
 ReactDOM.render(
-    renderRootElement(),
+    <App/>,
     document.getElementById("root")
 );
 
--- src/redux/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from "redux";
-import swimmingPool from "./modules/swimmingPool";
-
-export default combineReducers({
-    swimmingPool
-});
